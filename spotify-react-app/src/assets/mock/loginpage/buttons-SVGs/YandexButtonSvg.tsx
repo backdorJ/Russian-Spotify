9d86fad20369369471cbe55bd312b40ca2b8f71b
Marat Gaffarov <gaffarov142004@gmail.com>
2024-03-15 13:43:54 +0300

    отрефакторил LoginPage v8 отрефакторил RegisterPage v2 вынес svg в отдельные tsx (через img не работает)

---
+++ spotify-react-app/src/assets/mock/loginpage/buttons-SVGs/YandexButtonSvg.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const YandexButtonSvg = () => {
+    return (
+        <svg viewBox="-2 0 24 24">
+            <path
+                d="M13.32 7.666h-.924c-1.694 0-2.585.858-2.585 2.123 0 1.43.616 2.1 1.881 2.959l1.045.704-3.003 4.487H7.49l2.695-4.014c-1.55-1.111-2.42-2.19-2.42-4.015 0-2.288 1.595-3.85 4.62-3.85h3.003v11.868H13.32V7.666z"
+                fill="#fff"
+                className="yandex-button-svg"/>
+        </svg>
+    )
+}
