323f05de8305c4be71af46b08a20175ff848feb7
irekkkarimov <irekkkarimov@mail.ru>
2024-04-01 21:07:04 +0300

    made dynamic song list display

--- spotify-react-app/src/pages/PlaylistPage/PlaylistPage.tsx
+++ spotify-react-app/src/pages/PlaylistPage/PlaylistPage.tsx
@@ -9,12 +9,13 @@ import like_icon from "../../assets/mock/playlistpage/like.png"
 import options_icon from "../../assets/mock/playlistpage/options_icon.png"
 import {useContext, useEffect, useState} from "react";
 import {SpotifyContext} from "../../index";
+import SongCard from "./components/SongCard";
 
 const PlaylistPage = () => {
+    const sidebarWidth = 280
     const userStore = useContext(SpotifyContext)
     const [backgroundWidth, setBackgroundWidth] = useState(0)
     const [windowWidth, setWindowWidth] = useState(document.body.clientWidth)
-    const sidebarWidth = 280
     const playlistName = "Chill Mix"
     const singer1 = "Julia Wong"
     const singer2 = "ayokay"
@@ -26,6 +27,10 @@ const PlaylistPage = () => {
     const updateWindowWidth = () => {
         setWindowWidth(document.body.clientWidth)
     }
+
+    useEffect(() => {
+        updateWindowWidth()
+    }, []);
 
     useEffect(() => {
         window.onresize = updateWindowWidth
@@ -58,20 +63,58 @@ const PlaylistPage = () => {
                         </p>
                     </div>
                 </div>
-                    <div className="playlist-page__songs">
-                        <div className="playlist-page__songs__header">
-                            <div className="playlist-page__songs__header__buttons">
-                                <div className="playlist-page__songs__header__buttons__play">
-                                    <img src={play_icon} alt="Play"/>
-                                </div>
-                                <img className="playlist-page__songs__header__buttons__like" src={like_icon} alt="Like"/>
-                                <img className="playlist-page__songs__header__buttons__options" src={options_icon} alt="Options"/>
+                <div className="playlist-page__songs">
+                    <div className="playlist-page__songs__header">
+                        <div className="playlist-page__songs__header__buttons">
+                            <div className="playlist-page__songs__header__buttons__play">
+                                <img src={play_icon} alt="Play"/>
                             </div>
-                        </div>
-                        <div className="playlist-page__songs__list">
-
+                            <img className="playlist-page__songs__header__buttons__like" src={like_icon} alt="Like"/>
+                            <img className="playlist-page__songs__header__buttons__options" src={options_icon}
+                                 alt="Options"/>
                         </div>
                     </div>
+                    <div className="playlist-page__songs__list">
+                        <div className="playlist-page__songs__list__header">
+                            <div className="playlist-page__songs__list__header__id">
+                                <p>#</p>
+                            </div>
+                            <div className="playlist-page__songs__list__header__title">
+                                <p>TITLE</p>
+                            </div>
+                            <div className="playlist-page__songs__list__header__album">
+                                <p>ALBUM</p>
+                            </div>
+                            <div className="playlist-page__songs__list__header__added">
+                                <p>DATE ADDED</p>
+                            </div>
+                            <div className="">
+                                <p>LENGTH</p>
+                            </div>
+                        </div>
+                        <div className="playlist-page__songs__list__divider"></div>
+                        <div className="playlist-page__songs__list__main">
+                            <SongCard
+                                id="1"
+                                name="Slow Grenade"
+                                artists={['Ellie Goulding', 'Lauv']}
+                                album="Brightest Blue"
+                                length="3:37"/>
+                            <SongCard
+                                id="1"
+                                name="Slow Grenade"
+                                artists={['Ellie Goulding', 'Lauv']}
+                                album="Brightest Blue"
+                                length="3:37"/>
+                            <SongCard
+                                id="1"
+                                name="Slow Grenade"
+                                artists={['Ellie Goulding', 'Lauv']}
+                                album="Brightest Blue"
+                                length="3:37"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- spotify-react-app/src/pages/PlaylistPage/components/SongCard.tsx
+++ spotify-react-app/src/pages/PlaylistPage/components/SongCard.tsx
@@ -1,10 +1,45 @@
+// @ts-ignore
+import song_img from "../../../assets/mock/playlistpage/songs/slow_grenade_img.png"
+import {Fragment} from "react";
 
-
-const SongCard = () => {
+const SongCard = (props: any) => {
+    const {id, name, artists, album, length} = props
+    let artistCount = artists.length
+    let artistsArray = [...artists]
+    let artistsMapped = artistsArray.map((artist, index) => {
+        if (index < artistCount - 1)
+            return (<Fragment><span>{artist}</span>, </Fragment>)
+        return (<Fragment><span>{artist}</span></Fragment>)
+    })
 
     return (
-        <div className="playlist-page__song">
+        <div className="playlist-page__songs__list__main__song-card">
+            <div className="playlist-page__songs__list__main__song-card__id">
+                <p>{id}</p>
+            </div>
+            <div className="playlist-page__songs__list__main__song-card__title">
+                <img src={song_img} alt={name}
+                     className="playlist-page__songs__list__main__song-card__title__img"/>
+                <div className="playlist-page__songs__list__main__song-card__title__info">
+                    <div className="playlist-page__songs__list__main__song-card__title__info__song-name">
+                        <p>{name}</p>
+                    </div>
+                    <div className="playlist-page__songs__list__main__song-card__title__info__artist-names">
+                        <p>{artistsMapped}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="playlist-page__songs__list__main__song-card__album">
+                <p>{album}</p>
+            </div>
+            <div className="playlist-page__songs__list__main__song-card__added">
 
+            </div>
+            <div className="playlist-page__songs__list__main__song-card__length">
+                <p>{length}</p>
+            </div>
         </div>
     )
 }
+
+export default SongCard
