9a632e2d2764fd7ebc89339fc5c177e234e9e719
irekkkarimov <irekkkarimov@mail.ru>
2024-04-19 12:27:41 +0300

    trigger

---
+++ RussianSpotify.API/RussianSpotify.API.Data.PostgreSQL/Migrations/20240405175929_addedFileOnSongDeleteCascade.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RussianSpotift.API.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedFileOnSongDeleteCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Files_Songs_SongId",
+                table: "Files");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Files_Songs_SongId",
+                table: "Files",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Files_Songs_SongId",
+                table: "Files");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Files_Songs_SongId",
+                table: "Files",
+                column: "SongId",
+                principalTable: "Songs",
+                principalColumn: "Id");
+        }
+    }
+}
