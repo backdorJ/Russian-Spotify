3e8d35f847a6118ec5529e9fb1714f3029ae8c56
Marat Gaffarov <gaffarov142004@gmail.com>
2024-05-14 07:31:11 +0300

    рефакторинг, изменил иконку Liked Songs, пофиксил отображение плейлистов автора

--- spotify-react-app/src/commonComponents/Player/components/CloseExpandedPlayer.tsx
+++ spotify-react-app/src/commonComponents/Player/components/CloseExpandedPlayer.tsx
@@ -4,8 +4,8 @@ const CloseExpandedPlayer = (props: any) => {
     return (
         <svg onClick={props.onClick} width="48" height="48" viewBox="0 0 48 48" fill="none"
              xmlns="http://www.w3.org/2000/svg">
-            <path d="M18 18L24 24M24 24L30 30M24 24L30 18M24 24L18 30" stroke="white" stroke-opacity="0.5"
-                  stroke-width="2" stroke-linecap="round"/>
+            <path d="M18 18L24 24M24 24L30 30M24 24L30 18M24 24L18 30" stroke="white" strokeOpacity="0.5"
+                  strokeWidth="2" strokeLinecap="round"/>
         </svg>
     );
 };
